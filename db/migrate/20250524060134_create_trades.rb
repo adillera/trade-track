40bfcb8f5edc8cbd93be5e585ef4f9fe6762ee6d
adillera <aj.dillera@gmail.com>
2025-05-24 15:20:57 +0800

    Additional columns added.

--- db/migrate/20250524060134_create_trades.rb
+++ db/migrate/20250524060134_create_trades.rb
@@ -1,11 +1,17 @@
 class CreateTrades < ActiveRecord::Migration[8.0]
   def change
     create_table :trades do |t|
+      t.string :pair
       t.datetime :taken_at
       t.string :position
       t.decimal :lot_size
       t.string :confidence
       t.string :result
+      t.decimal :amount
+      t.decimal :profit
+      t.decimal :win_rate
+      t.decimal :loss_rate
+      t.decimal :profit_factor
 
       t.timestamps
     end
