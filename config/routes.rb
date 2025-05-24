ae8a25d747a6cda3f74e552c98c8287416b5ee7b
adillera <aj.dillera@gmail.com>
2025-05-24 12:48:18 +0800

    Dashboard controller and added tailwind-ruby

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
+  resource :dashboard, only: [:show]
+  root "dashboard#show"
   # root "posts#index"
 end
