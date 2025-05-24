b26f05b081610e7ce99d0c54e8c94c4c1aa22ac7
adillera <aj.dillera@gmail.com>
2025-05-24 15:40:32 +0800

    Used the latest trade for metrics.

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -4,29 +4,36 @@ class Trade < ApplicationRecord
 
   def self.metrics
     total = count
-    total_profit = sum(:profit_counter)
-    winning_profits = where(result: "win").sum(:profit_counter)
-    losing_profits = where(result: "loss").sum(:profit_counter).abs
+    latest_trade = order(created_at: :desc).first
     
     {
       total_trades: total,
-      win_rate: first&.win_rate_counter || 0,
-      loss_rate: first&.loss_rate_counter || 0,
-      total_profit: total_profit,
-      profit_factor: losing_profits.positive? ? (winning_profits / losing_profits) : 0
+      win_rate: latest_trade&.win_rate_counter || 0,
+      loss_rate: latest_trade&.loss_rate_counter || 0,
+      total_profit: latest_trade&.profit_counter || 0,
+      profit_factor: calculate_profit_factor
     }
   end
 
   private
 
+  def self.calculate_profit_factor
+    winning_profits = where(result: "win").sum(:profit_counter)
+    losing_profits = where(result: "loss").sum(:profit_counter).abs
+    losing_profits.positive? ? (winning_profits / losing_profits) : 0
+  end
+
   def update_profit_counter
-    self.profit_counter = if result == 'win'
-                           amount
-                         elsif result == 'loss'
-                           -amount
-                         else
-                           0
-                         end
+    previous_total = self.class.order(created_at: :desc).first&.profit_counter || 0
+    current_profit = if result == 'win'
+                      amount
+                    elsif result == 'loss'
+                      -amount
+                    else
+                      0
+                    end
+    
+    self.profit_counter = previous_total + current_profit
   end
 
   def update_rate_counters
